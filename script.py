8a60d5282bdd5266c14d4174983f73bb1534b834
Kevin Wittmer <wittmerkevinr@gmail.com>
2016-04-07 00:59:14 -0400

    extract English tweets

--- script.py
+++ script.py
@@ -1,10 +1,16 @@
 import json
 
 # Open the data and read each JSON object into a list
-data_file = open('sample_data.json', 'r')
+data_file = open('collection_brands.11.json', 'r')
 tweets = data_file.readlines()
+data_file.close()
 
-# Load each JSON object and print the tweetId
+# Load each JSON object and add English tweets to new list
+english_tweets = []
 for line in tweets:
     values = json.loads(line)
-    print values["tweetId"]
+    if values['tweetOwner']['language'] == 'en':
+        english_tweets.append(values)
+
+# Print the number of English tweets retrieved
+print len(english_tweets)
