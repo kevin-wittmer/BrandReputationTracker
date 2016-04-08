85e736626e971932121fea090b58d447119fd60a
Kevin Wittmer <wittmerkevinr@gmail.com>
2016-04-08 09:44:12 -0400

    added ability to accept command line args

--- clean.py
+++ clean.py
@@ -1,21 +1,33 @@
 import csv
 import json
+import sys
+
+# Check for proper command line arguments
+if len(sys.argv) != 2:
+    print 'Usage: clean.py <filename.json>'
+    sys.exit(2)
 
 # Open the data and read each JSON object into a list
-data_file = open('sample_data.json', 'r')
+data_file = open(sys.argv[1], 'r')
 tweets = data_file.readlines()
 data_file.close()
 
+# Parse the name of the csv file
+csv_filename = sys.argv[1]
+csv_filename = csv_filename[0:10] + csv_filename[18:20] + '.csv'
+
 # Open csv file to write data to
-with open('clean.csv', 'wb') as csvfile:
+with open(csv_filename, 'wb') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(['tweetId', 'text'])    # header
 
-    # Write each ID and tweet to csv file
+    # For each English tweets, write each ID and text to csv file
     for line in tweets:
         values = json.loads(line)
-        #if values['tweetOwner']['language'] == 'en':
-        writer.writerow([values['tweetId'],
-            values['text'].encode('ascii', 'ignore')])
+        if values['tweetOwner']['language'] == 'en':
+            writer.writerow([values['tweetId'],
+                values['text'].encode('ascii', 'ignore')])
 
+# Print location of cleaned data and close csv file
+print 'Data written to:', csvfile.name
 csvfile.close()
